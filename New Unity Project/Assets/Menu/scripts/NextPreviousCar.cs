26e01cb63b751578dec6a0d651172c90ced82fd8
jayamina <chandimajayamina94@gmail.com>
2018-06-18 11:43:58 +0530

    first commit

---
+++ New Unity Project/Assets/Menu/scripts/NextPreviousCar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPreviousCar : MonoBehaviour {
+	public GameObject[] cars;
+	private int currenti;
+	private int previousi;
+	// Use this for initialization
+	void Start () {
+		currenti = 0;
+		cars [currenti].SetActive (true);
+		//Instantiate (cars[currenti],new Vector3(0,0,0),Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+
+
+	public void NextCar(){
+		cars [currenti].SetActive (false);
+		if (currenti == cars.Length-1)
+			currenti = 0;
+		else
+			currenti++;
+		cars [currenti].SetActive (true);
+
+	}
+
+	public void PreviousCar(){
+		cars [currenti].SetActive (false);
+		if (currenti == 0) {
+			currenti = cars.Length - 1;
+		} else {
+			currenti--;
+		}
+		cars [currenti].SetActive (true);
+
+
+	}
+}
---
+++ New Unity Project/Assets/Menu/scripts/classButtonActivater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classButtonActivater : MonoBehaviour {
+	public GameObject[] classButtons;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickActivateButton(){
+		classButtons [0].SetActive (true);
+		classButtons [1].SetActive (true);
+
+	}
+}
